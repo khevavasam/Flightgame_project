dd9636f3f23530429b5dfd368000fb099b515764
Hovinarri <miki.pelkonen@gmail.com>
2025-09-22 14:14:55 +0300

    Add project level config file

---
+++ game/config.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
+DB_PORT = os.getenv("DB_PORT", 3306)
+DB_NAME = os.getenv("DB_NAME", "flight_game")

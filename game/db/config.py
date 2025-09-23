b61c36f6eb2cae82479fb6ba23abbd462d02f676
Hovinarri <miki.pelkonen@gmail.com>
2025-09-23 23:47:29 +0300

    Remove unnecessary comments

--- game/db/config.py
+++ game/db/config.py
@@ -2,9 +2,6 @@ import mysql.connector
 from contextlib import contextmanager
 from game import config
 
-# Yleiset database asetukset
-# user, password, host, port ladataan dotenv kirjaston avulla projektin .env filestä
-# charset, collation ja autocommit hard koodattu
 DB_CONFIG = {
     "user": config.DB_USER,
     "password": config.DB_PASSWORD,
@@ -17,8 +14,6 @@ DB_CONFIG = {
 }
 
 
-# Yleinen database connection methodi
-# https://docs.python.org/3/library/contextlib.html
 @contextmanager
 def get_connection():
     connection = mysql.connector.connect(**DB_CONFIG)

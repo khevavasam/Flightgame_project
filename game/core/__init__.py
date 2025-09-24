cac61525ba67b75d63f3605ad1e3ce5ee3392b61
Hovinarri <miki.pelkonen@gmail.com>
2025-09-24 18:21:07 +0300

    Update package exports

--- game/core/__init__.py
+++ game/core/__init__.py
@@ -1,4 +1,5 @@
 from .entities.airport import Airport
 from .game import Game
+from .events.game_event import WeatherEvent, WeatherType
 
-__all__ = ["Airport", "Game"]
+__all__ = ["Airport", "Game", "WeatherEvent", "WeatherType"]

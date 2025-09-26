b6febffaf1c0ab1913d7c2d6f184ef3ec562bc87
Hovinarri <miki.pelkonen@gmail.com>
2025-09-26 14:40:26 +0300

    Add renderer class to cli and draw map feature

---
+++ game/cli/renderer.py
@@ -0,0 +1,48 @@
+from typing import List
+from game.utils.math_helpers import clamp, scale_to_index, normalize
+
+# Finland min max coordinates for the map scaling
+MIN_LAT, MAX_LAT = 59.0, 70.0
+MIN_LON, MAX_LON = 20.0, 32.0
+
+
+class Renderer:
+    def __init__(self) -> None:
+        self.map_width = 40
+        self.map_height = 30
+
+    def _to_grid(self, lat: float, lon: float) -> tuple[int, int]:
+        # Horizontal.
+        x_ratio = normalize(lon, MIN_LON, MAX_LON)
+        x = scale_to_index(x_ratio, self.map_width)  # Map to column number.
+        x = clamp(x, 0, self.map_width - 1)
+        # Vertical.
+        y_ratio = normalize(lat, MIN_LAT, MAX_LAT)
+        y_ratio = 1 - y_ratio  # Flip y to make north top row.
+        y = scale_to_index(y_ratio, self.map_height)  # Map to row number.
+        y = clamp(y, 0, self.map_height - 1)
+
+        return x, y
+
+    def draw_map(self, current, target, airports: List) -> str:
+        map_grid = [[" "] * self.map_width for _ in range(self.map_height)]
+
+        # Place airports in grid.
+        for airport in airports:
+            x, y = self._to_grid(airport.lat, airport.lon)
+            map_grid[y][x] = "*"
+
+        # Mark player's current airport.
+        x, y = self._to_grid(current.lat, current.lon)
+        map_grid[y][x] = "@"
+
+        # Mark current quest target airport.
+        x, y = self._to_grid(target.lat, target.lon)
+        map_grid[y][x] = "X"
+
+        # TODO: Mark 5 nearest airports with &
+
+        return "\n".join("".join(row) for row in map_grid)
+
+    def clear_console(self):
+        print("\033[H\033[J", end="")
